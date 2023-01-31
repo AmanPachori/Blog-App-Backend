eef8a69d74f0208af1f6f43d01b5e0b79d93dbae
AmanPachori <amangirrajpachori2002@gmail.com>
2023-01-31 22:56:36 +0530

    model update

--- models/notes.model.js
+++ models/notes.model.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const moment = require('moment-timezone');
-const date = moment.tz(Date.now(), "Asia/Kolkata").format('DD-MM-YYYY HH:mm')
+const moment = require("moment-timezone");
+const date = moment.tz(Date.now(), "Asia/Kolkata").format("DD-MM-YYYY HH:mm");
 
 const noteSchema = new Schema({
-    title : {type:String},
-    mainContent : {type:String},
-    image: {type:String},
-    userId : {type:String},
-    likes :[{
-        type:String,
-    }],
-    category  : [{
-        type:String,
-    }],
-    createdOn : { type : String , default: date}
-    
-})
+  title: { type: String },
+  mainContent: { type: String },
+  image: { type: String },
+  userId: { type: String },
+  likes: [
+    {
+      type: String,
+    },
+  ],
+  category: [
+    {
+      type: String,
+    },
+  ],
+  createdOn: { type: String, default: date },
+});
 
-const notes  = mongoose.model('Notes', noteSchema);
+const notes = mongoose.model("Notes", noteSchema);
 module.exports = notes;
--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema   = new Schema({
-    username : {type:String},
-    email : {type:String},  
-    password : {type:String},
-    address : {type:String},
-    image : {type:String,
-    default:"https://xsgames.co/randomusers/assets/images/favicon.png"},
-    mobileNumber : {type:String},
-    likes : [{ type : String}],
-    notes : [{ type : String}]
-})
-const User = mongoose.model('User', userSchema);
+const userSchema = new Schema({
+  username: { type: String },
+  email: { type: String },
+  password: { type: String },
+  address: { type: String },
+  image: {
+    type: String,
+    default: "https://xsgames.co/randomusers/assets/images/favicon.png",
+  },
+  mobileNumber: { type: String },
+  likes: [{ type: String }],
+  notes: [{ type: String }],
+});
+const User = mongoose.model("User", userSchema);
 module.exports = User;
